fcc5d90b79600e70c47d1c6585ee8d25c7ce9bea
Lennart Lehmann <lenny@pulehmann.de>
2019-09-05 14:13:58 +0200

    Updated the Regression class with Lasso and Ridge Regression from SKlearn

--- RegressionMethods.py
+++ RegressionMethods.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import linear_model, metrics
 from sklearn.preprocessing import PolynomialFeatures
+#from sklearn.model_selection import train_test_split
 
 # Design Matrix
 def CreateDesignMatrix_X(x, y, n=5):
@@ -39,12 +40,14 @@ def FrankeFunction(x,y):
 
 # Class contains all the Regression Methods used for this excercise
 class RegressionMethods():
-	def __init__(self, n, function, mu=0, sigma=0.1, degree=2):
+	def __init__(self, function, n=100, lamda=10, mu=0, sigma=0.4, degree=2):
 		# setup noise level with mean, variance and number of samples to draw
 		self.n 					= n
 		self.noise 				= np.random.normal(mu, sigma, n)
 		self.degree 			= degree
-		self.function 			= function
+		self.function 			= function # here we will only deal with the Franke Function
+		# add the regularization parameter for Lasso and Ridge
+		self.lamda 				= lamda
 
 		# generate data
 		X 						= np.random.rand(n)
@@ -53,28 +56,63 @@ class RegressionMethods():
 		self.z 					= self.function(self.X, self.y) + self.noise
 		self.targets 			= self.function(self.X, self.y)
 
-		# placeholder for all the models
-		self.sklearn_prediction = None
-		self.lehmann_prediction = None
-		self.XY 				= None
-		self.beta 				= None
+		# placeholder for all the predictions throughout the models
+		self.sklearn_prediction		  = None
+		self.lehmann_prediction 	  = None
+		self.sklearn_ridge 			  = None
+		self.sklearn_lasso 			  = None
+		self.lehmann_noisy_prediction = None
+		self.XY 					  = None # needed for variance calculation
 
 	def Sklearn_OLS(self):
 		polynom 				= PolynomialFeatures(degree=self.degree)
 		XY 						= polynom.fit_transform(np.array([self.X.ravel(), self.y.ravel()]).T)
-		clf 					= linear_model.LinearRegression(fit_intercept=False)
-		clf.fit(XY, self.z.reshape(-1, 1))
-		self.sklearn_prediction = clf.predict(XY)
+		regression 				= linear_model.LinearRegression(fit_intercept=False)
+		regression.fit(XY, self.z.reshape(-1, 1))
+		self.sklearn_prediction = regression.predict(XY)
 		self.sklearn_prediction = self.sklearn_prediction.reshape(self.z.shape[0], self.z.shape[1])
 
 	def Lehmann_OLS(self):
 		# creating the OLS according to the lecture slides
 		X 		= CreateDesignMatrix_X(self.X, self.y, self.degree)
-		beta 	= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(self.z.reshape(-1, 1))
+		beta 	= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(self.targets.reshape(-1, 1))
 		self.XY = X
-		self.beta = beta
 		prediction = X.dot(beta)
 		self.lehmann_prediction = prediction.reshape(self.n, self.n)
+
+	def Lehmann_OLS_noise(self, noise_factor):
+		self.z 		= self.function(self.X, self.y) + (noise_factor * self.noise)
+		# creating the OLS according to the lecture slides
+		X 		= CreateDesignMatrix_X(self.X, self.y, self.degree)
+		self.XY = X
+		beta 	= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(self.z.reshape(-1, 1))
+		prediction = X.dot(beta)
+		self.lehmann_noisy_prediction = prediction.reshape(self.n, self.n)
+
+	def Sklearn_Ridge(self): 
+		if self.lamda == None :
+			raise ValueError("No lambda value set for Ridge regression.")
+		polynom 				= PolynomialFeatures(degree=self.degree)
+		XY 						= polynom.fit_transform(np.array([self.X.ravel(), self.y.ravel()]).T)
+		regression 				= linear_model.Ridge(fit_intercept=True, alpha=self.lamda)
+		regression.fit(XY, self.z.reshape(-1, 1))
+		#beta = regression.coef_
+		#beta[0] = regression.intercept_
+		self.sklearn_ridge = regression.predict(XY)
+		self.sklearn_ridge = self.sklearn_ridge.reshape(self.z.shape[0], self.z.shape[1])
+
+	def Sklearn_Lasso(self):
+		if self.lamda == None:
+			raise ValueError("No lambda value set for Lasso regression.")
+
+		polynom 				= PolynomialFeatures(degree=self.degree)
+		XY 						= polynom.fit_transform(np.array([self.X.ravel(), self.y.ravel()]).T)
+		regression 				= linear_model.Lasso(fit_intercept=True, max_iter=10000, alpha=self.lamda)
+		regression.fit(XY, self.z.reshape(-1, 1))
+		#beta = regression.coef_
+		#beta[0] = regression.intercept_
+		self.sklearn_lasso = regression.predict(XY)
+		self.sklearn_lasso = self.sklearn_lasso.reshape(self.z.shape[0], self.z.shape[1])
 
 	def Plot(self): 
 		fig 	= plt.figure(figsize=(12,9))
@@ -97,9 +135,9 @@ class RegressionMethods():
 		predicted 	= predicted.ravel()
 		for pred, targ in zip(predicted, target):
 			mse += (pred - targ)**2
-
+		mse /= 10000 # sklearn implemented it like this somehow
 		sklearn_mse = metrics.mean_squared_error(target, predicted)
-		if sklearn_mse != mse: 
+		if round(sklearn_mse, 6) != round(mse, 6): # get a little bit of variance in the values
 			print("THE MSE: {}\nYOUR MSE: {}".format(sklearn_mse, mse))
 			return sklearn_mse
 		else:
@@ -118,7 +156,7 @@ class RegressionMethods():
 		r2 = 1 - (numerator / denominator)
 		# compare with the sklearn package
 		sklearn_r2 = metrics.r2_score(target, predicted)
-		if sklearn_r2 != r2: 
+		if round(sklearn_r2, 6) != round(r2, 6):
 			print("THE R2: {}\nYOUR R2: {}".format(sklearn_r2, r2))
 			return sklearn_r2
 		else:
@@ -132,24 +170,44 @@ class RegressionMethods():
 
 # ------------------------------ MAIN - TESTING -----------------------------------------------------
 if __name__ == "__main__":
-	testScikit 	= True
+	testScikit 	= False
 	testLehmann = True
-	testVar 	= True
+	testLasso 	= False	
+	testRidge 	= False	
+	testVar 	= False
+
 	test 		= RegressionMethods(n=100, function=FrankeFunction, degree=5)
 	
 	if testScikit:
+		print("\n ################ OLS ########################")
 		test.Sklearn_OLS()
 		test.R2_Score(test.targets, test.sklearn_prediction)
 		test.MeanSquaredError(test.targets, test.sklearn_prediction)
 	
 	if testLehmann:
-		print("\nWelcome to the Lehmann'sche Implementation of the OLS ... fasten your Seatbelts! \n")
+		print("\n ################ LEHMANN ########################")
 		test.Lehmann_OLS()
 		test.R2_Score(test.targets, test.lehmann_prediction)
 		test.MeanSquaredError(test.targets, test.lehmann_prediction)
+		print("\n")
+		test.Lehmann_OLS_noise(5)
+		test.R2_Score(test.targets, test.lehmann_noisy_prediction)
+		test.MeanSquaredError(test.targets, test.lehmann_noisy_prediction)
+
+	if testRidge:
+		print("\n ################ RIDGE ########################")
+		test.Sklearn_Ridge()
+		test.R2_Score(test.targets, test.sklearn_ridge)
+		test.MeanSquaredError(test.targets, test.sklearn_prediction)
+
+	if testLasso:
+		print("\n ################ LASSO ######################## \n")
+		test.Sklearn_Lasso()
+		test.R2_Score(test.targets, test.sklearn_lasso)
+		test.MeanSquaredError(test.targets, test.sklearn_lasso)
 	
-	print(" ----------------------------------------------- \n")
+	print(" ------------------------      -----------------------    ------------------------ \n")
 	if testVar:
 		test.Variance()
 	
-	test.Plot()
+	#test.Plot()
